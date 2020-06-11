169edbb6e3dea95fc827844a69d0571fdf09b2aa
Mansab Uppal <muppal@thoughtworks.com>
2020-06-11 17:22:37 +0200

    release a new version

--- oktaauthpy3/metadata.py
+++ oktaauthpy3/metadata.py
@@ -6,13 +6,15 @@ Information describing the project.
 
 # The package name, which is also the "UNIX name" for the project.
 package = 'oktaauthpy3'
-project = "Okta CLI authentication"
+project = "Okta CLI authentication with Python3 support"
 project_no_spaces = project.replace(' ', '')
-version = '0.3'
+version = '0.4'
 description = 'Authenticates from the CLI'
 authors = ['Peter Gillard-Moss']
 authors_string = ', '.join(authors)
-emails = ['pgillard@thoughtworks.com']
+maintainers = ['Mansab Uppal']
+maintainers_string = ', '.join(maintainers)
+emails = ['muppal@thoughtworks.com']
 license = 'Apache 2.0'
 copyright = '2015 Thoughtworks Inc'
 url = 'https://www.thoughtworks.com/'
